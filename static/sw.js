9b8caa656a58201d122f9bfa97d2fdf211bef2d0
kunalagra <git@kunalag.simplelogin.com>
2023-05-16 01:41:54 +0530

    Fix Manifest & SW

--- static/sw.js
+++ static/sw.js
@@ -1,157 +1,54 @@
-class Pwa {
+// This is the service worker with the combined offline experience (Offline page + Offline copy of pages)
 
-    constructor(self) {
-        this.scope = self;
-        this.CACHE_VERSION = 1.4;
-        this.BASE_CACHE_FILES = [
-            '/ts/main.js',
-            '/404/',
-            'ts/search.js',
-            '/manifest.webmanifest',
-            '/favicon.ico',
-            '/offline/',
-            '/avatar.png',
-            '/about'
-        ];
-        this.host = `${self.location.protocol}//${self.location.host}`;
-        console.info(`Host: ${this.host}`);
-        this.OFFLINE_PAGE = '/offline/';
-        this.NOT_FOUND_PAGE = '/404/';
-        this.CACHE_NAME = `content-v${this.CACHE_VERSION}`;
-        this.MAX_TTL = 86400;
-        this.TTL_EXCEPTIONS = ["jpg", "jpeg", "png", "gif", "mp4"];
-    }
+const CACHE = "pwa-cache";
 
-    canCache(url) {
-        if (url.startsWith("http://localhost")) {
-            return false;
-        }
-        const result = url.toString().startsWith(this.host);
-        return result;
-    }
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-    getFileExtension(url) {
-        const extension = url.split('.').reverse()[0].split('?')[0];
-        return (extension.endsWith('/')) ? '/' : extension;
-    }
+// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
+const offlineFallbackPage = "offline/";
 
-    getTTL(url) {
-        if (typeof url === 'string') {
-            const extension = this.getFileExtension(url);
-            return ~this.TTL_EXCEPTIONS.indexOf(extension) ?
-                null : this.MAX_TTL;
-        }
-        return null;
-    }
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
 
-    async installServiceWorker() {
-        try {
-            await caches.open(this.CACHE_NAME).then((cache) => {
-                return cache.addAll(this.BASE_CACHE_FILES);
-            }, err => console.error(`Error with ${this.CACHE_NAME}`, err));
-            return this.scope.skipWaiting();
-        }
-        catch (err) {
-            return console.error("Error with installation: ", err);
-        }
-    }
+self.addEventListener('install', async (event) => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then((cache) => cache.add(offlineFallbackPage))
+  );
+});
 
-    cleanupLegacyCache() {
-
-        const currentCaches = [this.CACHE_NAME];
-
-        return new Promise(
-            (resolve, reject) => {
-                caches.keys()
-                    .then((keys) => keys.filter((key) => !~currentCaches.indexOf(key)))
-                    .then((legacy) => {
-                        if (legacy.length) {
-                            Promise.all(legacy.map((legacyKey) => caches.delete(legacyKey))
-                            ).then(() => resolve()).catch((err) => {
-                                console.error("Error in legacy cleanup: ", err);
-                                reject(err);
-                            });
-                        } else {
-                            resolve();
-                        }
-                    }).catch((err) => {
-                        console.error("Error in legacy cleanup: ", err);
-                        reject(err);
-                    });
-            });
-    }
-
-    async preCacheUrl(url) {
-        const cache = await caches.open(this.CACHE_NAME);
-        const response = await cache.match(url);
-        if (!response) {
-            return fetch(url).then(resp => cache.put(url, resp.clone()));
-        }
-        return null;
-    }
-
-    register() {
-        this.scope.addEventListener('install', event => {
-            event.waitUntil(
-                Promise.all([
-                    this.installServiceWorker(),
-                    this.scope.skipWaiting(),
-                ]));
-        });
-
-        this.scope.addEventListener('activate', event => {
-            event.waitUntil(Promise.all(
-                [this.cleanupLegacyCache(),
-                this.scope.clients.claim(),
-                this.scope.skipWaiting()]).catch((err) => {
-                    console.error("Activation error: ", err);
-                    event.skipWaiting();
-                }));
-        });
-
-        this.scope.addEventListener('fetch', event => {
-            event.respondWith(
-                caches.open(this.CACHE_NAME).then(async cache => {
-                    if (!this.canCache(event.request.url)) {
-                        return fetch(event.request);
-                    }
-                    const response = await cache.match(event.request);
-                    if (response) {
-                        const headers = response.headers.entries();
-                        let date = null;
-                        for (let pair of headers) {
-                            if (pair[0] === 'date') {
-                                date = new Date(pair[1]);
-                                break;
-                            }
-                        }
-                        if (!date) {
-                            return response;
-                        }
-                        const age = parseInt(((new Date().getTime() - date.getTime()) / 1000).toString());
-                        const ttl = this.getTTL(event.request.url);
-                        if (ttl === null || (ttl && age < ttl)) {
-                            return response;
-                        }
-                    }
-                    return fetch(event.request.clone()).then(resp => {
-                        if (resp.status < 400) {
-                            if (this.canCache(event.request.url)) {
-                                cache.put(event.request, resp.clone());
-                            }
-                            return resp;
-                        }
-                        else {
-                            return cache.match(this.NOT_FOUND_PAGE);
-                        }
-                    }).catch(err => {
-                        console.error(`Error fetching ${event.request.url} resulted in offline`, err);
-                        return cache.match(this.OFFLINE_PAGE);
-                    })
-                }));
-        });
-    }
+if (workbox.navigationPreload.isSupported()) {
+  workbox.navigationPreload.enable();
 }
 
-const pwa = new Pwa(self);
-pwa.register();
+workbox.routing.registerRoute(
+  new RegExp('/*'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: CACHE
+  })
+);
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith((async () => {
+      try {
+        const preloadResp = await event.preloadResponse;
+
+        if (preloadResp) {
+          return preloadResp;
+        }
+
+        const networkResp = await fetch(event.request);
+        return networkResp;
+      } catch (error) {
+
+        const cache = await caches.open(CACHE);
+        const cachedResp = await cache.match(offlineFallbackPage);
+        return cachedResp;
+      }
+    })());
+  }
+});
